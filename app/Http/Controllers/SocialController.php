79d00a3e4234e7228ce9466562b95b1c1d5fec93
Leonardo <dicapriofc2004@gamil.com>
2025-06-12 23:13:10 -0400

    Primer commit - proyecto Laravel

---
+++ app/Http/Controllers/SocialController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class SocialController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        $googleUser = Socialite::driver('google')->user();
+
+    
+        $user = User::firstOrCreate(
+            ['email' => $googleUser->getEmail()],
+            [
+                'name' => $googleUser->getName(),
+                'email_verified_at' => now(),
+                'password' => bcrypt(Str::random(10)),
+            ]
+        );
+    
+        Auth::login($user, true);
+    
+        return redirect('/dashboard');
+    }
+    
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
+
+
+use App\Http\Controllers\SocialController;
+
+Route::get('/login/google', [SocialController::class, 'redirectToGoogle']);
+Route::get('/login/google/callback', [SocialController::class, 'handleGoogleCallback']);
+
+
+require __DIR__.'/auth.php';
